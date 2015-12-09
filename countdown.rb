4e395fb6d90e0d76c59f5cf59ad7cba7b98bce76
DaniluR <ramirezdanilu6@gmail.com>
2015-12-09 01:06:41 +0000

    countdown-to-midnight

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
-
 def countdown
-end
+end 
+
+def countdown (phrase ="HAPPY NEW YEAR!")
+  number = 10 
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end 
+  "HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep (phrase ="HAPPY NEW YEAR!")
+  number = 10 
+  while number > 0 
+    sleep  1  
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
+end 
